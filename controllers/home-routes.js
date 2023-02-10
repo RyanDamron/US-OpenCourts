57b667a4044ce080884d17f7b133d45803e5cf00
jo3kr <joelmjohn16@gmail.com>
2023-02-10 12:56:08 -0600

    wrote login functionality and created handlebars pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 
 
+router.get('/', async(req,res)=>{
+    // Pass serialized data and session flag into template
+    res.render("homepage")
+    return;
+    } 
+);
+
 // SIGN UP
 router.get('/signup', async(req,res)=>{
   if (req.session.logged_in){
@@ -8,7 +15,7 @@ router.get('/signup', async(req,res)=>{
     return;
   }
     // Pass serialized data and session flag into template
-    res.render("signup")
+    res.render("loginpage")
     return;
     } 
 );
@@ -16,15 +23,44 @@ router.get('/signup', async(req,res)=>{
 // LOG IN
 router.get('/login', async(req,res)=>{
     if (req.session.logged_in){
-    //   res.redirect('/homepage')
+        res.redirect('/')
       return;
     }
       // Pass serialized data and session flag into template
-      res.render("login")
+      res.render("loginpage")
       return;
       } 
   );
-    module.exports = router;
+
+
+  // LOG IN
+router.get('/result', async(req,res)=>{
+  // if (!req.session.logged_in){
+  //     res.redirect('/login')
+  //   return;
+  // }
+    // Pass serialized data and session flag into template
+    res.render("resultpage")
+    return;
+    } 
+);
+
+router.get('/search', async(req,res)=>{
+  if (!req.session.logged_in){
+      res.redirect('/login')
+    return;
+  }
+    // Pass serialized data and session flag into template
+    res.render("searchpage")
+    return;
+    } 
+);
+
+
+
+
+
+ module.exports = router;
 
 
 
