e5a2606068c4288a61c134acce616a54a01a7323
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-02-13 16:51:17 -0600

    working on favorites not functioning yet

--- public/js/result.js
+++ public/js/result.js
@@ -1,24 +1,24 @@
-const signupFormHandler = async (event) => {
-    event.preventDefault();
+// const signupFormHandler = async (event) => {
+//     event.preventDefault();
   
-    const search = document.querySelector("#result").value.trim();
+//     const search = document.querySelector("#result").value.trim();
   
-    if (search) {
-      const response = await fetch("/api/court/searchedresult", {
-        method: "POST",
-        body: JSON.stringify({ search }),
-        headers: { "Content-Type": "application/json" },
-      });
+//     if (search) {
+//       const response = await fetch("/api/court/searchedresult", {
+//         method: "POST",
+//         body: JSON.stringify({ search }),
+//         headers: { "Content-Type": "application/json" },
+//       });
   
-      if (response.ok) {
-        alert("search found");
-        document.location.replace("/result");
-      } else {
-        alert("serach not found");
-      }
-    }
-  };
+//       if (response.ok) {
+//         alert("search found");
+//         document.location.replace("/result");
+//       } else {
+//         alert("serach not found");
+//       }
+//     }
+//   };
   
-  document
-    .querySelector(".search-form")
-    .addEventListener("submit", signupFormHandler);
+//   document
+//     .querySelector(".search-form")
+//     .addEventListener("submit", signupFormHandler);
